a5e72385daeb19e30f146532beec2aab90d91a50
NOIXLE <noixle.hh@gmail.com>
2025-05-08 15:50:38 -0600

    Delete hypr directory

--- hypr/Scripts/songdetail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-current_player=$(playerctl metadata --format '{{playerName}}')
-if [ "$current_player" == "spotify" ]; then
-    song_info=$(playerctl metadata --format ' {{title}} - {{artist}}')
-else
-    song_info=$(playerctl metadata --format ' {{title}} - {{artist}}')
-fi
-
-echo "$song_info" 
